a8da3cd47397c4d4cecfb13a79eff065111f20e2
Vishwajit1997 <vishwajitkumar37@gmail.com>
2022-07-01 12:56:43 +0530

    added simple char Device Driver

--- charDriver/charDriver_fops/charDev_fops.c
+++ charDriver/charDriver_fops/charDev_fops.c
@@ -14,11 +14,15 @@
 #include<linux/module.h>            // for module_init() & module_exit() MODULE_LICENSE() and so on...
 #include<linux/kdev_t.h>            // for MKDEV() , MAJOR(), MINOR()
 #include<linux/types.h>             // for different data types
-#include<linux/fs.h>                // for inode structure & File operations
-#include<linux/cdev.h>
+#include<linux/fs.h>                // for inode structure & File operations structure
+#include<linux/cdev.h>              // for cdev structure
 #include<linux/device.h>
-#include<linux/uaccess.h>
+#include<linux/uaccess.h>           // for copy_to_user and copy_from_user
+#include<linux/slab.h>              // for kmalloc() function
 
+#define NAME RW_CharDriver
+#define MEM_SIZE 1024
+uint8_t *kernel_buffer;
 
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("VISHWAJIT TIWARI");
@@ -32,10 +36,10 @@ static struct cdev my_cdev;             // For charDevice registration
 // Function prototypes
 static int      __init charDev_fops_init(void);
 static void     __exit charDev_fops_exit(void);
-static int      charDev_open(struct inode *inode, struct file *file);
-static int      charDev_release(struct inode *inode, struct file *file);
-static ssize_t  charDev_read(struct file *filp, char __user *buf, size_t len,loff_t * off);
-static ssize_t  charDev_write(struct file *filp, const char *buf, size_t len, loff_t * off);
+static int      charDev_open(struct inode *pinode, struct file *pfile);
+static int      charDev_release(struct inode *pinode, struct file *pfile);
+static ssize_t  charDev_read(struct file *filp, char __user *Ubuf, size_t len,loff_t * offp);
+static ssize_t  charDev_write(struct file *filp, const char *Ubuf, size_t len, loff_t * offp);
 
 
 // Structure for File Operations
@@ -53,8 +57,10 @@ struct file_operations fops =
  * @brief This function will be called when we open the Device file
  * 
  */
-static int charDev_open(struct inode *inode, struct file *file) {
+static int charDev_open(struct inode *pinode, struct file *pfile) 
+{
     pr_info("Driver Open function called....!!!\n");
+    printk(KERN_INFO "Device file opened....!!!\n");
     return 0;
 }
 
@@ -62,8 +68,11 @@ static int charDev_open(struct inode *inode, struct file *file) {
  * @brief This function will be called when we close the Device file
  * 
  */
-static int charDev_release(struct inode *inode, struct file *file) {
+static int charDev_release(struct inode *pinode, struct file *pfile) 
+{
     pr_info("Driver Release function called......!!!\n");
+    printk(KERN_INFO "Device File Closed...!!!\n");
+    
     return 0;
 }
 
@@ -71,100 +80,62 @@ static int charDev_release(struct inode *inode, struct file *file) {
  * @brief This function will be called when we Write to the Device file
  * 
  */
-static ssize_t charDev_write(struct file *filp, const char *buf, size_t len, loff_t *off) {
+static ssize_t charDev_write(struct file *filp, const char *Ubuf, size_t len, loff_t *offp) 
+{
     pr_info("Driver Write function called.........!!!\n");
-
-    unsigned long result1;
-    ssize_t retval;
-    char Kbuff[100];
-
     printk(KERN_ALERT "This is the kernel write call...Inside %s call\n", __FUNCTION__);
 
     // Access to user space data
-    result1 = copy_from_user((void *)Kbuff, (char *)buf, len);
-    if(result1 == 0) {
-        pr_info("Message from user app >>>%s<<<\n",Kbuff);
-        pr_info("Data received completely\n");
-        retval = len;
-        return retval;
+    if( copy_from_user(/*Kbuff*/kernel_buffer, Ubuf, len) ) {
+        pr_err("Data Write : Err!\n");
     }
-    else if(result1 > 0) {
-        pr_info("Message from user app >>>%s<<<\n", Kbuff);
-        pr_info("only some part of data is received\n");
-        retval = (len-result1);
-        return retval;
-    }
-    else {
-        pr_info("Error in write in kernel space\n");
-        retval = -EFAULT;
-        return retval;
-    }
-
-    return len;
+    printk("Data Write : Done!\n");;
+    
+    return len; 
 }
 
 /**
  * @brief This function will be called when we Read through Device File
  * 
  */
-static ssize_t charDev_read(struct file *filp, char __user *buf, size_t len, loff_t *off) {
+static ssize_t charDev_read(struct file *filp, char __user *Ubuf, size_t len, loff_t *offp) 
+{
     pr_info("Driver Read function called...........!!!\n");
-
-    unsigned long result;
-    ssize_t retval;
-    char Kbuff[100];
-
     printk(KERN_ALERT "This is the kernel read call...Inside %s call\n", __FUNCTION__);
 
     // Access to user space data
-    result = copy_to_user((void *)buf, (const void *)Kbuff, sizeof(Kbuff));
-    if(result == 0) {
-        pr_info("Message to user app >>>%s<<<\n",buf);
-        pr_info("Data sent completely\n");
-        retval = len;
-        return retval;
+    if( copy_to_user(Ubuf, kernel_buffer, MEM_SIZE) ) {
+        pr_err("Data Read : Err!\n");
     }
-    else if(result > 0) {
-        pr_info("Message to user app >>>%s<<<\n", buf);
-        pr_info("only some part of data has been sent\n");
-        retval = (len-result);
-        return retval;
-    }
-    else {
-        pr_info("Error in Read in kernel space\n");
-        retval = -EFAULT;
-        return retval;
-    }
+    pr_info("Data Read: Done!\n");
 
-    return len;
+    return MEM_SIZE;
 }
 
 
 /***********************************Start of Init Module**********************************************/
-static int __init charDev_fops_init(void) {
-
-    printk(KERN_ALERT "Inserting charDev_fops into Kernel\n");
-    /**
-     * @brief Allocating Major number Dynamically
-     * int alloc_chrdev_region(dev_t *dev, unsigned int firstminor, unsigned int count, char *name);
-     */
+static int __init charDev_fops_init(void) 
+{
+    printk(KERN_ALERT "Inside %s Module\n", __FUNCTION__);
+    printk(KERN_ALERT "Inserting Device Driver into Kernel\n");
+    
+    /*Allocating Major number Dynamically*/
     if(alloc_chrdev_region(&mydev,0,1,"charDev_fops") < 0) {
         pr_err("Cannot allocate Major number\n");
         return -1;
     }
+    pr_info("Device Number: Major = %d Minor = %d\n", MAJOR(mydev), MINOR(mydev));
 
-    pr_info("Device Number allocated successfully:\n Major = %d Minor = %d\n", MAJOR(mydev), MINOR(mydev));
-
-    // Creating cdev structure
+    /*Creating cdev structure*/
     cdev_init(&my_cdev, &fops);
 
-    // Adding charDevice to the system
+    /*Adding charDevice to the System*/
     if((cdev_add(&my_cdev,mydev,1)) < 0) {
         pr_err("Cannot add device to the syste\n");
-        return -1;
+        goto r_class;
     }
 
-    // Creating struct Class for Device
+    /*Creating struct Class for Device*/
     if((dev_class = class_create(THIS_MODULE,"charDev_class")) == NULL) {
         printk(KERN_ERR "Cannot create the struct class for device\n");
         goto r_class;
@@ -172,7 +143,7 @@ static int __init charDev_fops_init(void) {
 
     /**
      * @brief Creating Device File
-     * struct device *device_create (struct *class, struct device *parent, 
+     * @file struct device *device_create (struct *class, struct device *parent, 
      *                               dev_t dev, void * drvdata, const char *fmt, ...);
      */
     if((device_create(dev_class,NULL,mydev,NULL,"charDev_device")) == NULL) {
@@ -180,7 +151,15 @@ static int __init charDev_fops_init(void) {
         goto r_device;
     }
 
-    pr_alert("Kernel Module Inserted Successfully\n");
+    /*Creating Physical memory*/
+    if((kernel_buffer = kmalloc(MEM_SIZE, GFP_KERNEL)) == 0){
+        pr_info("Cannot allocate memory in kernel\n");
+        goto r_device;
+    }
+        
+    strcpy(kernel_buffer, "Hello_From_Kernel");
+
+    pr_alert("Device Driver Inserted... Successfully\n");
 
     return 0;
 
@@ -197,14 +176,16 @@ r_class:
 
 
 /***************************************Cleanup Module*********************************************/
-static void __exit charDev_fops_exit(void) {
-
+static void __exit charDev_fops_exit(void) 
+{
+    printk(KERN_ALERT "Inside %s Module\n", __FUNCTION__);
     printk(KERN_ALERT "Removing charDev_fops from Kernel\n");
-    cdev_del(&my_cdev);                             // To remove charDevice from the system
+    // kfree(kernel_buffer/*Kbuff*/);
     device_destroy(dev_class,mydev);                // To destroy char device 
     class_destroy(dev_class);                       // To destroy device class
+    cdev_del(&my_cdev);                             // To remove charDevice from the system
     unregister_chrdev_region(mydev,1);              // To unregister Device Number 
-    pr_info("Kernel Module Removed Successfully\n");
+    pr_info("Device Driver removed... Successfully\n");
 }
 /*************************************End of Cleanup Module****************************************/
 
